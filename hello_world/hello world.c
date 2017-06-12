500ce2e288f0d6f46b29243c4cc1509b1d31bb6c
brax2000 <37brax@gmail.com>
2017-06-12 11:22:58 -0500

    initial commit

---
+++ hello_world/hello world.c
@@ -0,0 +1,11 @@
+/* Braxton Bevis
+ * Program 1
+ * hello world
+ * 06/12/17
+ */
+#include "simpletools.h"                      
+
+int main()                                    // Main function
+{
+  print("Hello world");
+}
