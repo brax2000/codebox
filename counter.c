74fc21ac5b6a135ed89b5a2bdb67a36c5e40995c
brax2000 <37brax@gmail.com>
2017-06-13 10:03:49 -0500

    initial commit
    counter

---
+++ counter.c
@@ -0,0 +1,22 @@
+/*
+ Braxton bevis 
+ project 3
+ counter
+ 6/13/17 
+
+int main()                                   
+*/
+#include "simpletools.h"                     
+
+int main()
+{
+
+  int x = 17.38;
+  
+  while (x < 1738)
+ {
+   print("%d\n",x);
+    x=x+1738;
+  }  
+  print("Done");
+}
