07f3f9d35d796bcdda58f6ab53e491e8e0bfe4e5
brax2000 <37brax@gmail.com>
2017-06-14 10:39:36 -0500

    initialcommit

---
+++ coolbuttonsounds/coolbuttonsounds.c
@@ -0,0 +1,95 @@
+/*
+Braxton Bevis
+Buttons
+Project 5
+6/14/17
+*/
+#include "simpletools.h"                      
+
+int main()                                    
+{
+  
+  // Add startup code here.
+
+ 
+  while(1)
+  {
+    if( input(0) == 1 )
+    {
+      putChar (HOME) ;
+      print("Press button for light %c\n", CLREOL) ;
+  
+    }  
+    else
+    {
+      putChar (HOME) ;
+      print("ERROR......ERROR......ERROR....__________***%$#@!#$%^&*(((((((*&^%@#$%^ %c\n", CLREOL) ;
+      low(26);
+      pause(150);
+      high(27);
+      pause(15);
+      high(26);
+      pause(150);
+      low(27);
+      pause(15);
+      low(26);
+      pause(15);
+      high(27);
+      pause(150);
+      high(26);
+      pause(15);
+      low(27);
+      pause(150);
+       low(26);
+       pause(500);
+      pause(150);
+      high(27);
+      pause(15);
+      high(26);
+      pause(150);
+      low(27);
+      pause(15);
+      low(26);
+      pause(15);
+      high(27);
+      pause(150);
+      high(26);
+      pause(15);
+      low(27);
+      pause(150);
+         low(26);
+      pause(150);
+      high(27);
+      pause(15);
+      high(26);
+      pause(150);
+      low(27);
+      pause(15);
+      low(26);
+      pause(15);
+      high(27);
+      pause(150);
+      high(26);
+      pause(15);
+      low(27);
+      pause(150);
+       low(26);
+       pause(500);
+      pause(150);
+      high(27);
+      pause(15);
+      high(26);
+      pause(150);
+      low(27);
+      pause(15);
+      low(26);
+      pause(15);
+      high(27);
+      pause(150);
+      high(26);
+      pause(15);
+      low(27);
+      pause(150); 
+  }    
+  }  
+}
